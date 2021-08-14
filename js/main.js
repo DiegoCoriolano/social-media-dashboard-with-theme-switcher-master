7862f8c61fd229e452bfd106da7be1f5fc1f99cb
DiegoCoriolano <cdiego.dm@hotmail.com>
2021-08-13 21:49:21 -0300

    Atualizações
    • Utilização de classes no codigo HTML para a busca no JavaScript.
    • Inserção de variáveis no JavaScript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 const toggle = document.getElementById("themeMode");
 const background=document.getElementById("body");
 const topBg=document.getElementById("top-bg");
-const card=document.getElementsByClassName('social');
+var card=document.getElementsByClassName('social');
+var cardStats=document.getElementsByClassName('overview');
+var h1=document.getElementsByClassName("h1");
+var h5=document.getElementsByClassName("h5");
+var mode=document.getElementsByClassName("toggleText");
+var user=document.getElementsByClassName("user");
+var number=document.getElementsByClassName("number");
+var h2=document.getElementsByClassName("title-overview");
+
+
+
 
 function theme(){
     if (toggle.checked==true){
@@ -15,11 +25,52 @@ function theme(){
 function lightMode(){
     background.style.backgroundColor="white";
     topBg.style.backgroundColor="hsl(225, 100%, 98%)";
-card.style.backgroundColor="blue";!""
+    h1[0].style.color="hsl(230, 17%, 14%)";
+    h5[0].style.color="hsl(228, 12%, 44%)";
+    mode[0].style.color="hsl(228, 12%, 44%)";
+    h2[0].style.color="hsl(228, 12%, 44%)";
+
+    for(i=0; i<card.length; i++){
+        card[i].style.backgroundColor="hsl(227, 47%, 96%)";
+    }
+
+    for(i=0; i<cardStats.length; i++){
+        cardStats[i].style.backgroundColor="hsl(227, 47%, 96%)";
+    }
+
+    for(i=0; i<user.length; i++){
+        user[i].style.color="hsl(228, 12%, 44%)";
+    }
+
+    for(i=0; i<number.length; i++){
+        number[i].style.color="black";
+    }
 }
 
 function darkMode(){
     background.style.backgroundColor="hsl(230, 17%, 14%)";
     topBg.style.backgroundColor="hsl(232, 19%, 15%)";
+    h1[0].style.color="white";
+    h5[0].style.color="hsl(228, 34%, 66%)";
+    mode[0].style.color="white";
+    h2[0].style.color="white";
+
+    for(i=0; i<card.length;i++){
+        card[i].style.backgroundColor="hsl(228, 28%, 20%)";
+    }
+
+    for(i=0; i<cardStats.length;i++){
+        cardStats[i].style.backgroundColor="hsl(228, 28%, 20%)";
+    }
+
+    for(i=0; i<user.length; i++){
+        user[i].style.color="hsl(228, 34%, 66%)";
+    }
+
+    for(i=0; i<number.length; i++){
+        number[i].style.color="white";
+    }
 }
+
+
 
